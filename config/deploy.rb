2eae87d00cc85bc9ecad40184265cf63e06a540d
Preston Lee <conmotto@gmail.com>
2015-01-30 19:25:59 -0700

    Production bootstrap issues.

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/ca
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-set :keep_releases, 3
+set :keep_releases, 99
 
 namespace :deploy do
 
