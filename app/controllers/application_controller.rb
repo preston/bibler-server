63d7174aa52e64d1ef77daad66fb6d9d8149184e
Preston Lee <conmotto@gmail.com>
2015-01-30 22:42:30 -0700

    Allowing CORS.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :allow_cors
+
+  def allow_cors
+  	headers['Access-Control-Allow-Origin'] = '*'
+	headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+	headers['Access-Control-Request-Method'] = '*'
+	headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'  	
+  end
 end
