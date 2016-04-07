8412bb55d6bc35f0f7783b48f68f4fd63790bf20
Preston Lee <preston.lee@prestonlee.com>
2016-04-07 00:17:18 -0700

    Removing CSRF.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   before_filter :allow_cors
 
@@ -9,6 +10,6 @@ class ApplicationController < ActionController::Base
   	headers['Access-Control-Allow-Origin'] = '*'
 	headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
 	headers['Access-Control-Request-Method'] = '*'
-	headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'  	
+	headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
