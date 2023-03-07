63687c87362d01fe73bc516033bc03be4d78298e
Preston Lee <preston.lee@prestonlee.com>
2023-03-06 20:58:45 -0700

    CORS fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    # origins '*'
+    origins ["*", "https://localhost:4200", "https://bibler.prestonlee.com"]
+
 
     resource '*',
       headers: :any,
